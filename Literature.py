9edcf4f3d92bb03ae8a5172e549c043022a5cd90
Niyaz Valeev <niaz-valeev@mail.ru>
2023-05-01 14:42:07 +0300

    feat: добавлен класс Literature, хранящий информацию о литературе

---
+++ Literature.py
@@ -0,0 +1,16 @@
+"""
+Модель информации о литературе
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Literature:
+    name: str
+    autor: str
+    link: str
+    pk: int = 0
+
+    def get_text(self):
+        return "Название: " + self.name + ", Авторы: " + self.autor, self.link
